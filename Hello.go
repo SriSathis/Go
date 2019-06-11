de007d71d60981c66328566f218ab39cdaafed57
SriSathis <srisathishmsc@gmail.com>
2019-06-11 13:38:09 +0530

    Modified Hello.go & add the variable programs

--- Hello.go
+++ Hello.go
@@ -1,9 +1,11 @@
-package main  
- 
+package main
+
 import "fmt"
 
 func main() {
 
-     // prints geeksforgeeks
-     fmt.Println("Hello, World") 
+	// prints geeksforgeeks
+	fmt.Println("Hello, World")
 }
+
+// single line comment
