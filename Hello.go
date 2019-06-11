5f5bbb7ed9a225e627f9626a8c92d62ef15eef87
SriSathis <srisathishmsc@gmail.com>
2019-06-11 13:41:32 +0530

    Revert "Modified Hello.go & add the variable programs"
    This reverts commit de007d71d60981c66328566f218ab39cdaafed57.

--- Hello.go
+++ Hello.go
@@ -1,11 +1,9 @@
-package main
-
+package main  
+ 
 import "fmt"
 
 func main() {
 
-	// prints geeksforgeeks
-	fmt.Println("Hello, World")
+     // prints geeksforgeeks
+     fmt.Println("Hello, World") 
 }
-
-// single line comment
